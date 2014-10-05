7d629ae5ff5936cd2caee36d94b7a4fbff33685f
Omar Benitez <oomaar@live.com.mx>
2014-10-05 12:20:17 -0600

    Testeando el deploy

---
+++ app/models/Encuesta.java
@@ -0,0 +1,17 @@
+package models;
+
+import javax.persistence.Entity;
+import play.db.jpa.Model;
+
+/**
+ *
+ * @author Omar
+ * @since Oct 5, 2014
+ * @version 1
+ */
+@Entity
+public class Encuesta extends Model{
+    
+    
+    
+}
